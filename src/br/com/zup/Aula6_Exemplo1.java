1ef17256ad87008e3114a85974ea5bae8a21ad85
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-08-24 14:10:18 -0300

    Primeiro commit em Java

---
+++ src/br/com/zup/Aula6_Exemplo1.java
@@ -0,0 +1,21 @@
+package br.com.zup;
+
+public class Aula6_Exemplo1 {
+
+    public static void main (String[] args){
+
+        int idadeDoUsuario = 18;
+        String nomeDoUsuario = "Leticia";
+
+       System.out.println(nomeDoUsuario);
+       System.out.println(idadeDoUsuario);
+
+       //Printando de forma concatenada as variáveis
+
+        System.out.print("Olá, meu nome é " + nomeDoUsuario);
+
+
+    }
+
+}
+
