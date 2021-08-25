908ef7af6668ee89badd4b1cdfa1d6a459bf8d3d
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-08-24 23:37:27 -0300

    Resolução exercício 4 da Lista de Exercícios 001

---
+++ src/br/com/zup/ListaDeExercicios001_4.java
@@ -0,0 +1,11 @@
+package br.com.zup;
+
+public class ListaDeExercicios001_4 {
+    public static void main(String[] args) {
+        float salarioPorHora = 9.65F;
+        int horasTrabalhadasPorMes = 22 * 6;
+        float salarioTotalMensal = salarioPorHora * horasTrabalhadasPorMes;
+        System.out.printf( "%.2f",salarioTotalMensal);
+
+}
+    }
